e11eb4ec48139adcfa3a91c6fcd52ea205a2d1bb
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-08-01 10:37:41 +0100

    designed 95% of the desktop version and 50 % of the mobile version

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/* HAMBORGA NAVIGATION*/
+const ham = document.getElementById('navigation');
+const naviUl = document.getElementsByClassName('navUl');
+naviUl.classList.add('active')
+const mobileNav = document.getElementById('hide');
