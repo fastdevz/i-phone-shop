329500cbfb71de5c0cf11da297cda9ba7dc08c89
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-08-07 23:50:36 +0100

    added mobile page details

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ const hamClose = document.querySelector('.hamClose');
 ham.addEventListener('click',()=>{
 nav.classList.add('navActive');
 });
+
 hamClose.addEventListener('click',()=>{
     nav.classList.remove('navActive');
     });
-
+/*End of hamborger navigations*/
 
