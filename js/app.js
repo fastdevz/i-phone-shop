21bf8aff8a14516089e2986b5a3fec177958a206
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-08-12 14:23:48 +0100

    end of chalenge

--- js/app.js
+++ js/app.js
@@ -10,3 +10,63 @@ hamClose.addEventListener('click',()=>{
     nav.classList.remove('navActive');
     });
 /*End of hamborger navigations*/
+/* home pages click event for 3d display images */
+ const iframeData = [
+    {
+        name:'Huawei Mate 30 Black 3D Model',
+        tag:' <iframe width="745" height="480" src="https://360.hum3d.com/view/211995/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Huawei P20 Black 3D mode',
+        tag:' <iframe width="745" height="480" src="https://360.hum3d.com/view/203639/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Apple iPhone 13 Pro Gold 3D model',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/224126/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Appel Iphone 13 mini 3d model',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/219302/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Huawei Mate 30 Black 3D Model',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/94597/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Huawei P20 Black 3D mode',
+        tag:' <iframe width="745" height="480" src="https://360.hum3d.com/view/205909/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Apple iPhone 13 Pro Gold 3D model',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/217242/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Appel Iphone 13 mini 3d model',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/224117/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Huawei Mate 30 Black 3D Model',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/211589/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Huawei P20 Black 3D mode',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/205898/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Apple iPhone 13 Pro Gold 3D model',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/224126/" frameborder="0"></iframe>'
+    },
+    {
+        name:'Appel Iphone 13 mini 3d model',
+        tag:'<iframe width="745" height="480" src="https://360.hum3d.com/view/219302/" frameborder="0"></iframe>'
+    },
+ ];
+ const slideDisplay = (number)=>{
+    const result = Math.random(number).toPrecision(1).slice(2);
+    let num = Math.ceil(result);
+    return num;
+};
+   const dimesions = document.querySelector('#itemBackground');
+   dimesions.innerHTML = iframeData[slideDisplay(15)].tag;
+   /* End home pages click event for 3d display images */
+   
